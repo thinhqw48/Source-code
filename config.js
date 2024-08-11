d99cb84f5bf9bbaaf7467bfa39c041c00007af9c
thinhqw48 <140641198+thinhqw48@users.noreply.github.com>
2024-08-11 08:58:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = '\n';
+const giftUrl = '';
+const eventName = 'Nhấn để mở';
+const titleCard = 'Tặng người ấy';
+const contentCard = "Ghi lời chúc vào đây";
+const giftImage = 'images.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
